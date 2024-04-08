602025bb26a68d8cb6471b58fbec653ddaa8b6c0
Edinson <edisonprise@hotmail.com>
2024-04-07 22:14:16 -0500

    canvas config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client"
+
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="font-2xl text-white">Liveblocks Figma Clone</h1>
+    <div className="">
+      <Live />
     </div>
   );
 }
