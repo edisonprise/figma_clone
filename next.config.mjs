b64be8e8fceabc3c40db33026dc03f0073c09c36
Edinson <edisonprise@hotmail.com>
2024-04-12 17:59:32 -0500

    real time and history feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      sharp: "commonjs sharp",
+      canvas: "commonjs canvas",
+    });
+    return config;
+  },
   images: {
     remotePatterns: [
       {
